215443dd17c1fb391475709e02cf96495b1f85bb
sahlct <mhdsahlct@gmail.com>
2024-01-30 16:12:09 +0530

    js complete

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ let customor = [];
 let productname, productprice, customorname;
 let productobj;
 let count = 1;
-// let quantity =1;
+let secondbody;
 let orderedlist = [];
 let j = 0;
 let result = 0;
 let values = 1;
+let billname;
+// let quantity = 1;
+let index;
 
 document.getElementById("myform").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -60,7 +63,7 @@ document.getElementById("myformsecond").addEventListener("submit", function (eve
 function order(id) {
     let selectrow = document.getElementById(id);
     document.getElementById("table-responsive-second").style.display ="block"
-    let secondbody = document.getElementById("secondtablebody");
+    secondbody = document.getElementById("secondtablebody");
     let index;
     secondbody.innerHTML = ''
     console.log(selectrow);
@@ -96,14 +99,16 @@ function order(id) {
     for (let i = 0; i < orderedlist.length; i++) {
         let secondrow = document.createElement("tr")
         secondrow.className = "table-primary"
+        // let currentid = orderedlist[i].id
         secondrow.id = id
         secondrow.innerHTML = `
         <td scope="row">${orderedlist[i].name}</td>
         <td>${orderedlist[i].price}</td>
-        <td>${orderedlist[i].quantity}</td>
+        <td><input type="number" id='input${orderedlist[i].id}' class="quantityinput" value="${orderedlist[i].quantity}" onchange="quantitychange('${orderedlist[i].id}')"></td>
         <td>${orderedlist[i].amount}</td>
-        <td><button>Delete</button></td>
+        <td><button type="button" onclick="orderlist_delete('${orderedlist[i].id}')">Delete</button></td>
 `
+        
         secondbody.appendChild(secondrow)
 
     }
@@ -119,18 +124,16 @@ function order(id) {
 }
 
 function showbill() {
-    let billname = document.getElementById("optionname").value
-    if(billname == ''){
-        let alertModal = document.getElementById("alertmodaal");
-        alertModal.innerHTML =`
-        <div class="alert alert-info" role="alert">
-    This is a simple Bootstrap alert box.
-  </div>
+    billname = document.getElementById("optionname").value
+    console.log("billname value",billname);
+    if(billname === ''){
+        document.getElementById("modal-body").style.display ='none'
+        document.getElementById("modal-header").innerHTML = `
+        <h4>Please Choose a Customor Fiirst....!!</h4>
         `
-        document.getElementById("exampleModal").style.display ='none'
     }else{
-        document.getElementById("exampleModal").innerHTML =`
-        `
+        document.getElementById("modal-body").style.display ='block'
+      document.getElementById("modal-header").innerHTML =''  
     let billtbody = document.getElementById("billbody");
     billtbody.innerHTML = ''
     for (let l = 0; l < orderedlist.length; l++) {
@@ -148,8 +151,81 @@ function showbill() {
     document.getElementById("totalamount").innerHTML = `
     ${result}
     `
-   
-    // console.log(billname);
     document.getElementById("name").innerHTML = `Name : ${billname}`
 }
+}
+
+function orderlist_delete(id){
+    let deleteid = id
+    console.log(id);
+    orderedlist = orderedlist.filter(function (value){
+        return value.id !== deleteid
+    })
+    secondbody.innerHTML = ''
+    console.log("orderedlist : ",orderedlist);
+    for (let i = 0; i < orderedlist.length; i++) {
+        let secondrow = document.createElement("tr")
+        secondrow.className = "table-primary"
+        // let currentid = orderedlist[i].id
+        secondrow.id = id
+        secondrow.innerHTML = `
+        <td scope="row">${orderedlist[i].name}</td>
+        <td>${orderedlist[i].price}</td>
+        <td>${orderedlist[i].quantity}</td>
+        <td>${orderedlist[i].amount}</td>
+        <td><button type="button" onclick="orderlist_delete('${orderedlist[i].id}')">Delete</button></td>
+`
+        
+        secondbody.appendChild(secondrow)
+
+    }
+    result = 0
+
+    for (let m = 0; m < orderedlist.length; m++) {
+        result = orderedlist[m].amount + result
+    }
+    document.getElementById("amountshow").innerHTML = `
+    TOTAL AMOUNT OF PURCHASE : ${result}
+    `
+}
+
+function quantitychange (qid){
+    let inputvalue = document.getElementById(`input${qid}`)
+    if(inputvalue.value <= 0){
+        inputvalue.value = 1
+    }else {
+         let currentrow = orderedlist.filter(function (value,index1){
+        if(value.id === qid){
+            index = index1
+        }
+    })
+    orderedlist[index].quantity = parseInt(inputvalue.value)
+    orderedlist[index].amount = orderedlist[index].quantity * orderedlist[index].price
+    secondbody.innerHTML = ''
+    for (let i = 0; i < orderedlist.length; i++) {
+        let secondrow = document.createElement("tr")
+        secondrow.className = "table-primary"
+        // let currentid = orderedlist[i].id
+        secondrow.id = `${product[i].id}`
+        secondrow.innerHTML = `
+        <td scope="row">${orderedlist[i].name}</td>
+        <td>${orderedlist[i].price}</td>
+        <td><input type="number" id='input${orderedlist[i].id}' class="quantityinput" value="${orderedlist[i].quantity}" onchange="quantitychange('${orderedlist[i].id}')"></td>
+        <td>${orderedlist[i].amount}</td>
+        <td><button type="button" onclick="orderlist_delete('${orderedlist[i].id}')">Delete</button></td>
+`
+        
+        secondbody.appendChild(secondrow)
+
+    }
+    result = 0
+
+    for (let m = 0; m < orderedlist.length; m++) {
+        result = orderedlist[m].amount + result
+    }
+    document.getElementById("amountshow").innerHTML = `
+    TOTAL AMOUNT OF PURCHASE : ${result}
+    `
+    }
+   
 }
